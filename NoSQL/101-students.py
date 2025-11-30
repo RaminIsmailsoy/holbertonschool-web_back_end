69d0b4f68f9221fdd57fee0407683c275d8ce4b5
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-30 11:11:42 +0000

    all

--- NoSQL/101-students.py
+++ NoSQL/101-students.py
@@ -6,6 +6,25 @@ The top must be ordered  The average score must be part of each item returns wit
 
 
 def top_students(mongo_collection):
+    '''  Returns all students sorted by average score. Each student will have a new key 'averageScore'. '''
+
+    students = []
+    for student in mongo_collection.find():
+        scores = student.get('topics', [])
+        if scores:
+            # Calculate average of the 'score' values
+            avg = sum(topic['score'] for topic in scores) / len(scores)
+        else:
+            avg = 0
+        student['averageScore'] = round(avg, 2)  # match test formatting
+        students.append(student)
+
+    # Sort by averageScore descending
+    students.sort(key=lambda x: x['averageScore'], reverse=True)
+
+    return students
+
+def top_students(mongo_collection):
     ''' Returns all students sorted by average score '''
 
     return list(mongo_collection.find())
