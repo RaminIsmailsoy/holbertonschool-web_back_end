23857059f14a92f4b8bc4cc82f4f3f5f19d33362
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-30 11:27:24 +0000

    all

--- NoSQL/102-log_stats.py
+++ NoSQL/102-log_stats.py
@@ -7,27 +7,29 @@ from pymongo import MongoClient
 
 
 def log_stats():
+    ''' Define a function '''
+
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
 
-    # Total logs
+    ''' Total logs '''
     total_logs = logs.count_documents({})
     print(f"{total_logs} logs")
 
-    # Methods
+    ''' Methods '''
     print("Methods:")
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         count = logs.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    # Status check
+    ''' Status check '''
     status_count = logs.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_count} status check")
 
-    # Top 10 IPs using aggregation
+    ''' Top 10 IPs using aggregation '''
     pipeline = [
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1, "_id": 1}},  # sort by count desc, then IP asc
+        {"$sort": {"count": -1, "_id": 1}},
         {"$limit": 10}
     ]
     top_ips = logs.aggregate(pipeline)
@@ -35,7 +37,6 @@ def log_stats():
     print("IPs:")
     for ip in top_ips:
         print(f"\t{ip['_id']}: {ip['count']}")
-
 
 if __name__ == "__main__":
     log_stats()
